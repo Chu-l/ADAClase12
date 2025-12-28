413ead244a7e006a28cd2d74da2a023552927909
Lilen <lilen.chu@hotmail.com>
2025-12-27 21:13:45 -0300

    Ejercicio 1 terminado

---
+++ actividades.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+/*** Ejercicio 1: While
+Crea un programa que solicite al usuario ingresar números continuamente hasta que el usuario ingrese un número negativo. Luego, imprime la suma de todos los números ingresados. ***/
+
+let num = Number(prompt("Ingrese un numero, ingrese uno negativo para terminar: "));
+let suma = 0;
+
+while (num >= 0) {
+    suma += num;
+    num = Number(prompt("Ingrese otro numero (ingrese uno negativo para terminar): "));
+};
+
+console.log("La suma total es: ", suma);
--- prueba.js
+++
@@ -1,2 +0,0 @@
-dcfsdfsd
-ejercicio hecho
